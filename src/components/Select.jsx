a1b954c384b293d00bd666a5d2a09b24505e65c2
j-yesung <hisung-ah@kakao.com>
2023-11-17 13:23:29 +0900

    Select 컴포넌트 변수명 혼동으로 인해 수정

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,4 +1,4 @@
-import { selectedData } from 'modules/fanLetter';
+import { selectedData as selectDataAtion } from 'modules/fanLetter';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -6,26 +6,21 @@ const Select = () => {
   const fanLatter = useSelector(state => state.fanLetter);
   const dispatch = useDispatch();
 
-  // const selectedData = useSelector(state => state.select.data);
-  // const { selectedOption, setSelectedOption } = useContext(SelectedContext);
-
   const handleSelectChange = e => {
     const selectedName = e.target.value;
     const selectedData = fanLatter.data.find(item => item.name === selectedName);
+    console.log('selectedData: ', selectedData);
 
-    if (selectedName === '') return alert('이름을 선택해 주세요.');
-
-    // setSelectedOption(selectedData);
-    dispatch(selectedData(selectedData));
+    dispatch(selectDataAtion(selectedData));
   };
 
   useEffect(() => {
-    if (fanLatter.data.length) dispatch(selectedData(fanLatter.data[0]));
+    if (fanLatter.data.length) dispatch(selectDataAtion(fanLatter.data[0]));
   }, [fanLatter.data, dispatch]);
 
   return (
     <>
-      보낼 사람 선택 :
+      <p style={{ marginBottom: '20px' }}>보낼 사람 선택</p>
       <select value={fanLatter.selectedData ? fanLatter.selectedData.name : ''} onChange={handleSelectChange}>
         {fanLatter.data.map(item => {
           return <option key={item.id}>{item.name}</option>;
