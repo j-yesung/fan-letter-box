9292f3e0f4d2adccd439cdcb4b990d62417a7412
j-yesung <hisung-ah@kakao.com>
2023-11-18 17:24:56 +0900

    add redux Devtools

--- src/config/store.js
+++ src/config/store.js
@@ -1,10 +1,11 @@
 import { createStore } from 'redux';
 import { combineReducers } from 'redux';
 import fanLetter from 'modules/fanLetter';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const rootReducer = combineReducers({
   fanLetter,
 });
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
