1401138f4fc3826a7df573390ce0dbf63b172bf1
j-yesung <hisung-ah@kakao.com>
2023-11-20 13:01:54 +0900

    원상 복구

--- src/App.jsx
+++ src/App.jsx
@@ -15,10 +15,6 @@ const App = () => {
     dispatch(getJsonData(fakeData));
   }, [dispatch]);
 
-  if (window.location.href.indexOf('/fan-letter-box/') !== -1) {
-    window.location.href = '/fan-letter-box/';
-  }
-
   return (
     <>
       <ThemeProvider theme={fanLetter.isMode || lightTheme}>
